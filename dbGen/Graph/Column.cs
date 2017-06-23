d3b45ce5ede7cefbca75fd4e3534356e0b7beb98
Jack Kerr <jack775544@gmail.com>
2017-06-23 17:02:19 +1000

    Added some basic templates

--- dbGen/Graph/Column.cs
+++ dbGen/Graph/Column.cs
@@ -1,29 +1,64 @@
-using dbGen;
+using System.Collections.Generic;
 
-namespace dbGen.Net
+namespace dbGen
 {
+    /**
+    * A Column in the database. This column is not a foreign key.
+    */
     public class DatabaseColumn
     {
+        // The name of the column
         public string Name {get; set;}
-        public DataGenerator DataType {get; set;}
-        public DatabaseTable ReferenceTable {get; set;}
-        public DatabaseColumn ReferenceColumn {get; set;}
-        public bool RandomValue {get; set;}
+        // The generator of the data for the column
+        public DataGenerator Generator {get; set;}
+        // Is this column a primary key
         public bool PrimaryKey {get; set;}
+        // Is this column unique
         public bool Unique {get; set;}
+        // Is the column not null
         public bool NotNull {get; set;}
+        // The string version of the data of this column
+        public List<string> Data {get; set;}
+        // Has this column had it's data already set
+        public bool HasData {get; set;}
         
-        public DatabaseColumn(string name, DataGenerator dataType)
+        public DatabaseColumn(string name, DataGenerator dataGenerator)
         {
-            
+            // Set name and generator
+            Name = name;
+            Generator = dataGenerator;
+
+            // The table starts off empty
+            Data = null;
+            HasData = false;
+
+            // By default no special properties
+            PrimaryKey = false;
+            Unique = false;
+            NotNull = false;
         }
     }
 
+    /**
+    * A column in the database. This column is a foreign key
+    */
     public class DerivedDatabaseColumn : DatabaseColumn
     {
-        public DerivedDatabaseColumn(string name, DataGenerator dataType) : base(name, dataType)
+        // The table where the foreign key is located
+        public DatabaseTable ReferenceTable {get; set;}
+        // The column which the foreign key refers to.
+        public DatabaseColumn ReferenceColumn {get; set;}
+        // Does this column randomly select values from it's reference or sequentially
+        public bool RandomValue {get; set;}
+
+        /**
+        * Constructor for the foreign key column. The constructor for this column sets the parent data generator to false since 
+        * it refers to the data in it's reference table.
+        */
+        public DerivedDatabaseColumn(string name, DatabaseTable referenceTable, DatabaseColumn referenceColumn) : base(name, null)
         {
-            
+            ReferenceTable = referenceTable;
+            ReferenceColumn = referenceColumn;
         }
     }
 }
--- dbGen/Graph/Table.cs
+++ dbGen/Graph/Table.cs
@@ -1,10 +1,68 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
 namespace dbGen
 {
     public class DatabaseTable
     {
-        public DatabaseTable()
+        // The length of the table in rows
+        public int TableLength {get; set;}
+        // The name of the table
+        public string TableName {get; set;}
+        // The column list of the table
+        public List<DatabaseColumn> Columns {get; set;}
+        
+        /**
+        * The constructor for the database table
+        */
+        public DatabaseTable(string tableName, int tableLength, List<DatabaseColumn> columns)
         {
-            return;
+            TableName = tableName;
+            TableLength = tableLength;
+            Columns = new List<DatabaseColumn>(columns);
+        }
+
+        public int ColumnCount()
+        {
+            return Columns.Count;
+        }
+
+        /**
+        * Returns true iff the table has foreign keys
+        */
+        public bool HasReferences()
+        {
+            return Columns.Any(x => x is DerivedDatabaseColumn);
+        }
+
+        /**
+        * Gets any tables referenced by the foreign keys in the table
+        */
+        public List<DatabaseTable> GetReferencedTables()
+        {
+            return Columns.Where(x => x is DerivedDatabaseColumn)
+                .Select(x => (DerivedDatabaseColumn) x)
+                .Select(x => x.ReferenceTable)
+                .ToList();
+        }
+
+        /**
+        * Adds a list of <column name -> data> rows to the table. If no data is set for a column in the row then set to null.
+        * If a not null column is set to null then throw an error.
+        */
+        public bool AddData(List<Dictionary<string, string>> data)
+        {
+            foreach (var row in data)
+            {
+                // CLone our row since we are going to mutate further down the function.
+                var rowClone = new Dictionary<string, string>(row);
+                foreach (var col in rowClone)
+                {
+                    
+                }
+            }
+            return false;
         }
     }
 }
